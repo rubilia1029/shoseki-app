bb331e0d41a443f40e3e4c3876f7a817ef398a04
rubilia1029 <69662402+rubilia1029@users.noreply.github.com>
2020-10-12 11:16:38 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :profile])
   end
 end
